ae446548344ba2a7ccae3ac46ac384b0274edc01
reitoria <reitoria@R19001756>
2025-03-19 20:06:12 -0400

    Atualização da classe GreetingsController para criação dos END-POINTS de listar todos e inserir usuários

--- springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/controllers/GreetingsController.java
@@ -1,10 +1,17 @@
 package br.com.springboot.curso_jdev_treinamento.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,5 +49,21 @@ public class GreetingsController {
     	usuarioRepository.save(usuario);
     	
     	return "Ola mundo " + nome;
+    }
+    
+    @GetMapping(value = "listatodos")
+    @ResponseBody /*Retorna os dados para o corpo da Resposta*/
+    public ResponseEntity<List<Usuario>> listaUsuario(){
+    	List<Usuario> usuarios = usuarioRepository.findAll();
+    	
+    	return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK); /*Retorna a lista de usuarios em formato JSON*/
+    }
+    
+    @PostMapping(value = "salvar") /*Mapeia a URL*/
+    @ResponseBody /*Descrição da resposta*/
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario){
+    	Usuario user = usuarioRepository.save(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
 }
