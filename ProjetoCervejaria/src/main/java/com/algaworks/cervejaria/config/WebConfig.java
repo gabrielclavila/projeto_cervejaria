58338cc147b0a03248a4e8db2bcccd49a04d6aad
reitoria <reitoria@R19001756>
2025-08-13 11:51:22 -0400

    Alteração no pom.xml para importação do Thymeleaf e alteração na classe WebConfig para comunicação com a camada de View da arquitetura

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/WebConfig.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/WebConfig.java
@@ -1,9 +1,20 @@
 package com.algaworks.cervejaria.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.algaworks.cervejaria.controller.CervejariaController;
 
@@ -11,6 +22,37 @@ import com.algaworks.cervejaria.controller.CervejariaController;
 //@ComponentScan("com.algaworks.cervejaria.controller") - Poderia ser feito setado o pacote do controller como uma string, mas fica sucetível a erros de digitação
 @ComponentScan(basePackageClasses = { CervejariaController.class}) //basePackageClasses nós estamos pedindo para encontrar todas as classes com notação de @Controller no mesmo pacote da classe que colocamos o nome, dentro {} é um vetor podemos colocar outros nomes de classes
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter{
+public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware{
 
+	private ApplicationContext applicationContext;
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	
+	private ITemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+	}
+	
+	@Bean
+	public TemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
 }
