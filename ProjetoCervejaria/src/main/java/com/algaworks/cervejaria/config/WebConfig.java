10f95bf6d22290c04155cb466903f2747d5f00c8
reitoria <reitoria@R19001756>
2025-08-13 11:07:05 -0400

    Alterações na classe WebConfig e criação de uma classe de Controller

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/WebConfig.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/WebConfig.java
@@ -1,5 +1,16 @@
 package com.algaworks.cervejaria.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.algaworks.cervejaria.controller.CervejariaController;
+
+@Configuration
+//@ComponentScan("com.algaworks.cervejaria.controller") - Poderia ser feito setado o pacote do controller como uma string, mas fica sucetível a erros de digitação
+@ComponentScan(basePackageClasses = { CervejariaController.class}) //basePackageClasses nós estamos pedindo para encontrar todas as classes com notação de @Controller no mesmo pacote da classe que colocamos o nome, dentro {} é um vetor podemos colocar outros nomes de classes
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
 
 }
---
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/CervejariaController.java
@@ -0,0 +1,8 @@
+package com.algaworks.cervejaria.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CervejariaController {
+
+}
