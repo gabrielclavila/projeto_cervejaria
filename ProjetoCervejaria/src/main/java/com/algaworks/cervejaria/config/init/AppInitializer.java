c87d43d0ae54d5cbfbae71979fe0624b9ea8fdcd
reitoria <reitoria@R19001756>
2025-08-13 10:47:16 -0400

    Sobrescrita dos métodos da Classe AppInitializer e criação de uma nova classe WebConfig

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/init/AppInitializer.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/init/AppInitializer.java
@@ -2,24 +2,23 @@ package com.algaworks.cervejaria.config.init;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import com.algaworks.cervejaria.config.WebConfig;
+
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Class<?>[] {WebConfig.class};
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
-		return null;
+		return new String[] {"/"};
 	}
 
 }
