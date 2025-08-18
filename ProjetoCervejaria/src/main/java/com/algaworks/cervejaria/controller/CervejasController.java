c193035a29a607bec82c649307eed6c5e9621a6d
reitoria <reitoria@10.100.0.105>
2025-08-18 16:06:09 -0400

    Alteracao no CervejaController para inclusao do Redirect, e emissão de mensagem na pagina html, alteracao na pagina html para inclusao do thymeleaf e impressao da mensagem

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/CervejasController.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/CervejasController.java
@@ -3,9 +3,11 @@ package com.algaworks.cervejaria.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cervejaria.model.Cerveja;
 
@@ -19,13 +21,15 @@ public class CervejasController {
 	
 	//Ao invés de passar uma lista de parâmetros caso o formulário de cadastro possua vários com o ideal é sempre passar o Objeto Cerveja para o método
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result) {
+	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
-			System.out.println("Tem erro sim");
+			model.addAttribute("mensagem", "Erro no Formulário");
+			return "cerveja/CadastroCerveja"; //forward, redireciona para uma página
 		}
 		
+		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso"); //Cria uma sessão, para manter na mesma página mesmo com o redirect para poder exibir a mensagem na página
 		System.out.println("SKU: "+ cerveja.getSku());
-		return "cerveja/CadastroCerveja";
+		return "redirect:/cervejas/novo"; //redirect, redireciona para uma URL
 	}
 	
 }
