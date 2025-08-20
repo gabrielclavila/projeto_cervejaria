9ba760e8835c2c39ec743355ab0c804a62a95a2e
reitoria <reitoria@10.100.0.5>
2025-08-20 15:41:00 -0400

    Atualização referente a correção de problema de enconding, alterando para UTF-8 para resolver problema de acentuação

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/init/AppInitializer.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/config/init/AppInitializer.java
@@ -1,5 +1,8 @@
 package com.algaworks.cervejaria.config.init;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import com.algaworks.cervejaria.config.WebConfig;
@@ -19,6 +22,16 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	//Mesmo configurando o encoding UTF-8 no Preferences/Workspace ainda pode ocorrer em outras máquinas Windows o problema encoding (acentuação), esse método vai força a mudança para UTF-8
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		
+		return new Filter[] {characterEncodingFilter};
 	}
 
 }
