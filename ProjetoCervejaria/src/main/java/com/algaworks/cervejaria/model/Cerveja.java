a1d1e689b61fb09ca239e236c6347053fc79fcb5
reitoria <reitoria@10.100.0.5>
2025-08-20 15:25:29 -0400

    Atualização referente a resolução do desafio 3.9-Validando a Descrição

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/model/Cerveja.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/model/Cerveja.java
@@ -1,13 +1,20 @@
 package com.algaworks.cervejaria.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotBlank;
 
 public class Cerveja {
 	
+	@NotBlank
 	private String sku;
-	private String nome;
 	
 	@NotBlank
+	private String nome;
+	
+	@Size(min = 1, max = 50)
+	private String descricao;
+	
 	public String getSku() {
 		return sku;
 	}
@@ -19,6 +26,12 @@ public class Cerveja {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
 	}
 	
 	
