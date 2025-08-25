9b6735bd5e15cea2fd0a469cead71be9bf27a30e
reitoria <reitoria@R19001756>
2025-08-25 17:07:28 -0400

    Criação do prototipo da pagina de cadastro de usuario e criacao da classe UsuarioController para fazer o RequestMapping e poder visualizar o prototipo da pagina no navegador

---
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/UsuarioController.java
@@ -0,0 +1,19 @@
+package com.algaworks.cervejaria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.algaworks.cervejaria.model.Cerveja;
+
+@Controller
+public class UsuarioController {
+	
+	@RequestMapping("usuarios/novo")
+	public String novo(Model model) {
+		model.addAttribute(new Cerveja());
+		return "usuario/CadastroUsuario";
+	}
+	
+
+}
