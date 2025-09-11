361e7adc379872d07ffd92b1259e89495fd9e755
reitoria <reitoria@R19001756>
2025-09-11 13:53:12 -0400

    Atualização referente a tornar o combobox de Sabor dinâmico, pegando os valores do Enum Sabor e exibindo na pagina CadastroCerveja

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/CervejasController.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/CervejasController.java
@@ -2,40 +2,38 @@ package com.algaworks.cervejaria.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.cervejaria.model.Cerveja;
-import com.algaworks.cervejaria.repository.Cervejas;
+import com.algaworks.cervejaria.model.Sabor;
 
 @Controller
 public class CervejasController {
 	
-	@Autowired
-	private Cervejas cervejas; //Injeção de Dependência da Interface Cervejas do Repository
 	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Model model) {
-		//model.addAttribute(new Cerveja());
-		cervejas.findAll();
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		return mv;
 	}
 	
 	//Ao invés de passar uma lista de parâmetros caso o formulário de cadastro possua vários com o ideal é sempre passar o Objeto Cerveja para o método
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
-			return "cerveja/CadastroCerveja"; //forward, redireciona para uma página
+			return novo(cerveja); //forward, redireciona para uma página
 		}
 		
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso"); //Cria uma sessão, para manter na mesma página mesmo com o redirect para poder exibir a mensagem na página
 		System.out.println("SKU: "+ cerveja.getSku());
-		return "redirect:/cervejas/novo"; //redirect, redireciona para uma URL
+		return new ModelAndView("redirect:/cervejas/novo"); //redirect, redireciona para uma URL
 	}
 	
 }
