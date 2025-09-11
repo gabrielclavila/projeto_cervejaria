80e322f12e4b9629952bbc73c127da13bc32ec5d
reitoria <reitoria@R19001756>
2025-09-11 16:23:07 -0400

    Atualização referente a mudança no gerenciamento da transação, o primeiro insert no Banco a Transação estava Automática, abrindo e fechando a transação sempre, foram feitas alterações no JPAConfig e no CadastroCervejaService para que possamos gerenciar as transações manualmente

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/service/CadastroCervejaService.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/service/CadastroCervejaService.java
@@ -2,6 +2,7 @@ package com.algaworks.cervejaria.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.cervejaria.model.Cerveja;
 import com.algaworks.cervejaria.repository.Cervejas;
@@ -12,6 +13,7 @@ public class CadastroCervejaService {
 	@Autowired
 	private Cervejas cervejas;
 	
+	@Transactional
 	public void salvar(Cerveja cerveja) {
 		cervejas.save(cerveja);
 	}
