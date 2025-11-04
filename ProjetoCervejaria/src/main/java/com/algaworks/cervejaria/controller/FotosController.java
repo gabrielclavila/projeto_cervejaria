84df8a601df78980481fd54c8a7a91fb5b174a23
reitoria <reitoria@10.100.0.11>
2025-11-04 11:02:37 -0400

    Atualização referente a criação de duas novas colunas na tabela Cerveja no BD e na classe Model, criação da classe FotoDTO para auxiliar no armazenamento da foto, utilização do FotoDTO no FotosController e FotoStorageRunnable e alteração na pagina CadastroCerveja para receber e enviar os dados do nome e contentType das imagens

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/FotosController.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/FotosController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.cervejaria.dto.FotoDTO;
 import com.algaworks.cervejaria.storage.FotoStorageRunnable;
 
 @RestController //Foi utilizado RestController ao invés de só Controller porque a requisição está sendo feita via Ajax no action do Javascript do CadastroCerveja 
@@ -14,8 +15,8 @@ import com.algaworks.cervejaria.storage.FotoStorageRunnable;
 public class FotosController {
 
 	@PostMapping
-	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files) {
-		DeferredResult<String> resultado = new DeferredResult<>();
+	public DeferredResult<FotoDTO> upload(@RequestParam("files[]") MultipartFile[] files) {
+		DeferredResult<FotoDTO> resultado = new DeferredResult<>();
 		
 		Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
 		thread.start();
