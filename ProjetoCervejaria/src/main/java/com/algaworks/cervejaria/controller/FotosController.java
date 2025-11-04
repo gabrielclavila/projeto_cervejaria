2ecacc1f30360098dd56660774d954a6be99af77
reitoria <reitoria@10.100.0.11>
2025-11-04 09:58:27 -0400

    Atualização referente a utilização de Thread no FotosController e criação do FotoStorageRunnable para melhoria da disponibilidade da aplicação com retorno assincrono para evitar problemas quando vários usuários estiverem fazendo upload de imagens ao mesmo tempo

--- ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/FotosController.java
+++ ProjetoCervejaria/src/main/java/com/algaworks/cervejaria/controller/FotosController.java
@@ -4,15 +4,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.cervejaria.storage.FotoStorageRunnable;
 
 @RestController //Foi utilizado RestController ao invés de só Controller porque a requisição está sendo feita via Ajax no action do Javascript do CadastroCerveja 
 @RequestMapping("/fotos")
 public class FotosController {
 
 	@PostMapping
-	public String upload(@RequestParam("files[]") MultipartFile[] files) {
-		System.out.println(">>> files: " + files[0].getSize());
-		return "Ok!";
+	public DeferredResult<String> upload(@RequestParam("files[]") MultipartFile[] files) {
+		DeferredResult<String> resultado = new DeferredResult<>();
+		
+		Thread thread = new Thread(new FotoStorageRunnable(files, resultado));
+		thread.start();
+		
+		return resultado;
 	}
 }
